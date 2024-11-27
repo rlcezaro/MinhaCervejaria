9803f5405ba8289e7c302dc1cc481ae106551ca7
Rangel Cezaro <rangelcezaro@gmail.com>
2024-11-27 11:47:52 -0300

    Foi criado a page home e corrigido a navegação do menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,5 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 import { CervejasComponent } from './pages/cervejas/cervejas.component';
 import { FabricantesComponent } from './pages/fabricantes/fabricantes.component';
 import { ClientesComponent } from './pages/clientes/clientes.component';
@@ -7,8 +8,9 @@ import { EstoqueComponent } from './pages/estoque/estoque.component';
 import { BuscaComponent } from './pages/busca/busca.component';
 import { NavigationComponent } from './pages/navigation/navigation.component';
 
-export const routes: Routes = [
-  { path: '', component: NavigationComponent },
+export const routes: Routes = [  
+  { path: '', component: HomeComponent },
+  { path: 'navigation', component: NavigationComponent },
   { path: 'cervejas', component: CervejasComponent },
   { path: 'fabricantes', component: FabricantesComponent },
   { path: 'clientes', component: ClientesComponent },
