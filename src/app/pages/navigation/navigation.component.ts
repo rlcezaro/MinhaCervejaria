53ff6a35b26dae8473b2fe20d9150739bec6ae08
Rangel Cezaro <rangelcezaro@gmail.com>
2024-11-27 12:04:25 -0300

    Ajustado responsividade do menu navigation

--- src/app/pages/navigation/navigation.component.ts
+++ src/app/pages/navigation/navigation.component.ts
@@ -1,11 +1,26 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterModule } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
 
 @Component({
   selector: 'app-navigation',
-  imports: [MatToolbarModule, RouterModule],
+  imports: [
+    MatToolbarModule,
+    RouterModule,
+    MatIconModule,
+    MatButtonModule,
+    MatMenuModule,
+  ],
   templateUrl: './navigation.component.html',
   styleUrl: './navigation.component.css',
 })
-export class NavigationComponent {}
+export class NavigationComponent {
+  isMenuOpen = false;
+
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+}
