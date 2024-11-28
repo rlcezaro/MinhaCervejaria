3483a9390fe106ef2d0b3f8c9c81cc9a05b5a0a0
Rangel Cezaro <rangelcezaro@gmail.com>
2024-11-28 16:05:12 -0300

    Funcionalidade basica menu Relatorio

--- src/app/pages/relatorios/relatorios.component.ts
+++ src/app/pages/relatorios/relatorios.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { Cliente } from '../../models/cliente.models';
+import { Cerveja } from '../../models/cerveja.model';
+import { Estoque } from '../../models/estoque.model';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-relatorios',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule, CommonModule, HttpClientModule],
+  providers: [ApiService],
   templateUrl: './relatorios.component.html',
-  styleUrl: './relatorios.component.css'
+  styleUrls: ['./relatorios.component.css']
 })
-export class RelatoriosComponent {
+export class RelatoriosComponent implements OnInit {
+  clientes: Cliente[] = [];
+  cervejas: Cerveja[] = [];
+  estoques: Estoque[] = [];
 
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit() {
+    this.loadClientes();
+    this.loadCervejas();
+    this.loadEstoques();
+  }
+
+  loadClientes() {
+    this.apiService.getClientes().subscribe((clientes) => {
+      this.clientes = clientes;
+    });
+  }
+
+  loadCervejas() {
+    this.apiService.getCervejas().subscribe((cervejas) => {
+      this.cervejas = cervejas;
+    });
+  }
+
+  loadEstoques() {
+    this.apiService.getEstoques().subscribe((estoques) => {
+      this.estoques = estoques;
+    });
+  }
 }
